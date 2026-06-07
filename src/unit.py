466437e89d2019cf360a9b3c461d4d32069e226c
pfeatherstone <pfeatherstone@pf>
2026-06-07 15:52:26 +0100

    more test

--- src/unit.py
+++ src/unit.py
@@ -1,7 +1,6 @@
-import pytest
 import torch
 import onnxruntime as ort
-from   model import Hubert, export_onnx
+from   model import Hubert, export_onnx, distilhubert_official, load_pretrained
 
 
 @torch.inference_mode()
@@ -13,3 +12,14 @@ def test_export():
     out0   = net(x) 
     out1,  = netOrt.run(None, {'audio': x.numpy()})
     torch.testing.assert_close(out0, torch.from_numpy(out1))
+
+
+@torch.inference_mode()
+def test_official():
+    net0 = distilhubert_official().eval()
+    net1 = Hubert().eval()
+    load_pretrained(net1, net0)
+    audio = torch.randn(1, 48000)
+    out0 = net0(audio).last_hidden_state
+    out1 = net1(audio[None])
+    torch.testing.assert_close(out0,out1)
